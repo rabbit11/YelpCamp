03dbdd3fd1e6acca643ae1d86b38a414dcb5d59a
rabbit11 <pedro.coelho@dcomp.sor.ufscar.br>
2019-09-04 16:54:26 -0300

    visual updates

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -19,13 +19,14 @@ router.get("/", function (req, res) {
 router.post("/", middleware.isLoggedIn,function (req, res) {
     var name = req.body.name;
     var image = req.body.image;
+    var price = req.body.price;
     var description = req.body.description;
     var author = {
         id: req.user._id,
         username: req.user.username
     }
 
-    var newCampground = { name: name, image: image, description: description,
+    var newCampground = { name: name, image: image, price: price, description: description,
          author: author};
 
 
